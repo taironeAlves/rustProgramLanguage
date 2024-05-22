bb26cb2de11a0f3fc5e27f1fe13d8394ca9bd428
Tairone <tairone.alves@gmail.com>
2024-05-22 14:51:42 -0300

    220524_1451

--- hello_word/src/main.rs
+++ hello_word/src/main.rs
@@ -1,9 +1,30 @@
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("Erro ao passar argumentos"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Use um gcd Number...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("O Maior divisor comum de {:?}, é {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
+
     while m != 0 {
         if m < n {
             let t = m;
@@ -14,3 +35,17 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     }
     n
 }
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
+
+/*
+ fn main() {
+    println!("Hello, world!");
+}
+
+ */
